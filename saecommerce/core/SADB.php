f86ff269dee7945ce6ffcb011985fb6ce2cf4714
trinculescu <trinculescu>
2007-01-14 10:33:36 +0000

    initial

---
+++ saecommerce/core/SADB.php
@@ -0,0 +1,12 @@
+<?php
+//SADB based on PEAR::DB
+final class SADB extends DB {
+	private $instances;
+	public static function &singleton($dsn = DSN){
+		$md5Dsn = md5($dsn);
+		if (!is_a(self::$instances[$md5Dsn], 'DB')) {
+			self::$instances[$md5Dsn] = self::factory($md5Dsn);
+		}
+		return self::$instances[$md5Dsn];
+	}
+}
